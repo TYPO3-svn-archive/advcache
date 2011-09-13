2a67d577b64074f49a4cdaeda95d97a6ceb548ce
fabriziobranca <fabriziobranca@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-09-13 07:30:43 +0000

    Fixed syntax error
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/advcache/trunk@51871 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,7 +2,7 @@
 
 if (t3lib_extMgm::isLoaded('nc_staticfilecache')) {
 	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['nc_staticfilecache/class.tx_ncstaticfilecache.php']['createFile_initializeVariables'][] = 'EXT:advcache/Classes/Hooks/NcStaticFileCache.php:Tx_Advcache_Hooks_NcStaticFileCache->createFile_initializeVariables';
-	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['advcache/Classes/Api.php']['clearCacheByIdAndParameters'][] = 'EXT:advcache/Classes/Hooks/NcStaticFileCache.php:Tx_Advcache_Hooks_NcStaticFileCache->clearCacheByIdAndParameters'
+	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['advcache/Classes/Api.php']['clearCacheByIdAndParameters'][] = 'EXT:advcache/Classes/Hooks/NcStaticFileCache.php:Tx_Advcache_Hooks_NcStaticFileCache->clearCacheByIdAndParameters';
 }
 
 ?>
